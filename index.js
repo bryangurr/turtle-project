6016ca5ee9561005b80f18641c26b9ae87ae60fc
bryangurr <150076870+bryangurr@users.noreply.github.com>
2024-12-02 15:41:31 -0700

    Add generic get routes to different pages

--- index.js
+++ index.js
@@ -30,5 +30,76 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/donate', (req, res) => {
+  res.render('donate');
+});
+
+app.get('/jens_story', (req, res) => {
+  res.render('jens_story');
+});
+
+app.get('/sponsors', (req, res) => {
+  res.render('sponsors');
+});
+
+app.get('/volunteer', (req, res) => {
+  res.render('volunteer');
+});
+
+app.get('/schedule_event', (req, res) => {
+  res.render('schedule_event');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/employee_home', (req, res) => {
+  res.render('employee_home');
+});
+
+app.get('/manage_employees', (req, res) => {
+  res.render('manage_employees');
+});
+
+app.get('/create_employee', (req, res) => {
+  res.render('create_employee');
+});
+
+app.get('/edit_employee', (req, res) => {
+  res.render('edit_employee');
+});
+
+app.get('/manage_volunteers', (req, res) => {
+  res.render('manage_volunteers');
+});
+
+app.get('/edit_volunteer', (req, res) => {
+  res.render('edit_volunteer');
+});
+
+app.get('manage_events', (req, res) => {
+  res.render('manage_events');
+});
+
+app.get('edit_event', (req, res) => {
+  res.render('edit_event');
+});
+
+app.get('create_event', (req, rew) => {
+  res.render('create_event');
+});
+
+app.get('report_event', (req, res) => {
+  res.render('report_event');
+});
+
+
+
+
 // should always come last
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}`)); // Start listening
