b55332745f849a349a77b8b6b215e484049194ca
purpledragon89 <rileywtaylor@gmail.com>
2024-12-03 15:57:10 -0700

    https

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d turtleshelterprojectintex.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email bgurr13@byu.edu
+sudo certbot -n -d turtleshelterprojectintex4-6.us-east-1.elasticbeanstalk.com --nginx --agree-tos --email bgurr13@byu.edu
 # sudo certbot -n -d turtleshelter46.is404.net --nginx --agree-tos --email bgurr13@byu.edu
